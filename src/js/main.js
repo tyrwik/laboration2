6ddb112f9aab1cc387ba7539b4bfc221e22589cd
tyrwik <tywi2501@student.miun.se>
2026-02-18 15:50:06 +0000

    Skapat funktion för att rendera in info i tabellen

--- src/js/main.js
+++ src/js/main.js
@@ -13,10 +13,26 @@ async function loadData() {
     //Anropa och läs ut data
     try {
         const response = await fetch(url)
-        const courses = await response.json();
+        courses = await response.json();
         displayCourses(courses);
     } catch (error){
         console.error("fel: " + error);
     }
 }
 
+function displayCourses(courses) {
+    const tableBody = document.getElementById("courses");
+
+    tableBody.innerHTML = "";
+
+    courses.forEach(course => {
+        const row = document.createElement("tr");
+
+        row.innerHTML= `
+            <td>${course.code}</td>
+            <td>${course.coursename}</td>
+            <td>${course.progression}</td>
+        `
+        tableBody.appendChild(row);
+    });
+}
